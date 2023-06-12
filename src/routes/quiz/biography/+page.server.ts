8fc19819d6d0911d13b05a5dfd2a472ad69cbf80
Eduard von Briesen <eduard.von-briesen@adesso.de>
2023-06-12 13:08:45 +0200

    feat: :sparkles: added stats to bio quiz

--- src/routes/quiz/biography/+page.server.ts
+++ src/routes/quiz/biography/+page.server.ts
@@ -1,5 +1,7 @@
+import { db } from '$lib/firebase/firebase';
 import { getArtistInfoById, getArtistInfo } from '$lib/server/last-fm';
 import { fail } from '@sveltejs/kit';
+import { doc, getDoc, type DocumentData, setDoc } from 'firebase/firestore';
 
 export const load = async ({ cookies }) => {
 	const artist = 'Daft Punk';
@@ -33,12 +35,15 @@ export const load = async ({ cookies }) => {
 export const actions = {
 	default: async ({ request, cookies }) => {
 		const artistId = JSON.parse(cookies.get('current_quiz') || '{}').artist;
-		const answer = (await request.formData()).get('answer') as string;
+		const response = await request.formData();
 
 		const artist = await getArtistInfoById(artistId);
 		const artistName = artist.artist.name;
 
+		const answer = response.get('answer') as string;
 		const correctAnswer = answer.toLowerCase() === artistName.toLowerCase();
+
+		updateUser(correctAnswer, response.get('user_id') as string);
 
 		if (correctAnswer) {
 			return fail(200, { correct: correctAnswer, artist: artistName });
@@ -46,4 +51,25 @@ export const actions = {
 			return fail(200, { correct: correctAnswer, artist: artistName });
 		}
 	}
+};
+
+const updateUser = async (correct: boolean, user_id: string) => {
+	const userRef = doc(db, 'users', user_id);
+	const userDoc = await getDoc(userRef);
+	if (!userDoc.exists()) return;
+
+	const userData = userDoc.data() as DocumentData;
+	const newCorrect = (userData.stats?.correct || 0) + (correct ? 1 : 0);
+	const newIncorrect = (userData.stats?.incorrect || 0) + (correct ? 0 : 1);
+
+	await setDoc(
+		userRef,
+		{
+			stats: {
+				correct: newCorrect,
+				incorrect: newIncorrect
+			}
+		},
+		{ merge: true }
+	);
 };
