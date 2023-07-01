a492097c38d2e9b5d079bc96e903615354805c61
nickwiele <nickwiele@icloud.com>
2023-07-01 16:27:00 +0200

    first draft of algorithmus created and UserData extended

--- src/lib/firebase/dataBaseLoadings.ts
+++ src/lib/firebase/dataBaseLoadings.ts
@@ -1,5 +1,13 @@
 import { doc, getDoc,updateDoc,type DocumentData, Timestamp } from "firebase/firestore";
 
+const MAX_HISTORY_LENGTH: number = 20;
+const WEIGHT_QUESTIONS_OVERALL = 1;
+const WEIGHT_SCORE_OVERALL = 1.5;
+const WEIGHT_SCORE_HISTORY = 4;
+const WEIGHT_QUESTIONS_HISTORY = 3.5;
+
+
+
 export type UserData = {
     name: string;
     email: string;
@@ -16,10 +24,10 @@ export type UserData = {
       };
       music_period_scores: {
         outdated: number;
-        eighties: number;
-        nineties: number;
-        twothousandties: number;
-        charts: number;
+        "80s": number;
+        "90s": number;
+        "00s": number;
+        "2020s": number;
       };
       genres: {
         [genre: string]: {
@@ -77,7 +85,7 @@ export type UserData = {
     };
   };
 
-export type LevelData = {
+type LevelData = {
     levelOne: {
         correct: number;
         questions: number;
@@ -93,7 +101,19 @@ export type LevelData = {
     overall_questions: number;
 }
 
-type Genre = "rock" | "pop" | "jazz" | "folk_music" | "classic"| "rap" ;
+export type Genre = "rock" | "pop" | "jazz" | "folk_music" | "classic"| "rap" ;
+
+export type Levels = 1 |2|3;
+
+type GenreData = {
+    overallScore: number;
+    overallQuestions: number;
+    overallGenreScore: number ;
+    overallGenreQuestions: number;
+    historyGenreScore: number;
+    currentQuestionsInHistory: number;
+
+}
 
 export const saveHistory = async (docName: string, db: any)=>{
     const collectionsName = "users";
@@ -372,7 +392,7 @@ export const initDataStructure = (name: string, email: string )=> {
 
             },
             shortterm_genre_history:{
-                MAX_HISTORY_LENGTH: 20, // proposal =20
+                MAX_HISTORY_LENGTH: MAX_HISTORY_LENGTH, // proposal =20
                 current_index: -1, // index of the oldest element, if overall_questions<20 then index=-1
                 list_of_genre: [] //list of genres of the last MAX_HISTORY_LENGTH questions
             }
@@ -406,33 +426,94 @@ export async function getGenreWithLevelForItem(docName: string, db: any){
     }
 
     const userData = docSnap.data() as UserData;
-    const genre: string = getGenreForItemtype(userData);
+    const genre: Genre = getGenreForItemtype(userData);
 
-    const genreLevelData = userData.progress.genres[genre] as LevelData;
-    const level = getLevelForGenre(genre, genreLevelData);
-    return {genre, level};
+    const genreLevelData:LevelData = userData.progress.genres[genre] as LevelData;
+    const level:Levels = getLevelForGenre(genreLevelData) as Levels;
+    return {
+        genre: genre, 
+        level: level
+    };
 }
 
 function getGenreForItemtype(data: UserData){
     const genres: Genre[] = ["rock", "pop", "jazz", "rap", "classic", "folk_music"]
-
-    for (const genre in genres){
-        
+    let scores: number[] = [];
+    let score: number = 0;
+    let historyLength: number;
+    if (data.progress.shortterm_genre_history.current_index===-1){
+        historyLength = data.progress.shortterm_genre_history.list_of_genre.length;
+    }else{
+        historyLength = MAX_HISTORY_LENGTH;
     }
-    return genres[0];
+    for (const genre in genres){
+        const genreData: GenreData=extractRelevantGenreData(data, genre as Genre);
+        if (genreData.overallQuestions===0){ // first question ever
+            score = Math.random();
+        }else if (genreData.overallScore===0) { // no question rightfully answers 
+            score =   WEIGHT_QUESTIONS_OVERALL * genreData.overallGenreQuestions/genreData.overallQuestions
+                    + WEIGHT_QUESTIONS_HISTORY * genreData.currentQuestionsInHistory/historyLength;
+                    + 0.5*Math.random();
+        }else{
+            score =   WEIGHT_QUESTIONS_OVERALL * genreData.overallGenreQuestions/genreData.overallQuestions
+                    + WEIGHT_SCORE_OVERALL * genreData.overallGenreScore/genreData.overallScore
+                    + WEIGHT_SCORE_HISTORY * genreData.historyGenreScore
+                    + WEIGHT_QUESTIONS_HISTORY * genreData.currentQuestionsInHistory/historyLength
+                    + 0.1*Math.random();
+        }
+        scores.push(score);   
+    }
+    return genres[scores.indexOf(Math.min(...scores))];
 }
 
-function getLevelForGenre(genre: string, genreLevelData: LevelData){
+/**
+ * Function to determin the level of the next quetsions (of type genre)
+ * @returns {Levels} The sum of the two numbers
+ */
+function getLevelForGenre(genreLevelData: LevelData){
+    const level1_score:number = genreLevelData.levelOne.correct/genreLevelData.levelOne.questions;
+    const level2_score:number = genreLevelData.levelTwo.correct/genreLevelData.levelTwo.questions;
+    const level3_score:number = genreLevelData.levelThree.correct/genreLevelData.levelThree.questions;
+    const diff1_2: number = level1_score-level2_score;
+    const diff1_3: number = level1_score-level3_score;
+    const diff2_3: number = level2_score-level3_score;
 
+    if (level1_score < 0.2){
+        return 1;
+    }
+    if (level2_score < 0.4){
+        let scores: number[] = []; 
+        scores.push(Math.random()+level1_score+diff1_2);
+        scores.push(Math.random()+level2_score-diff1_2);
+        return scores.indexOf(Math.min(...scores))+1;
+    }else{
+        let scores: number[] = []; 
+        scores.push(0.3*Math.random()+level1_score+0.5*diff1_2+0.3*diff1_3);
+        scores.push(0.5*Math.random()+level2_score-0.5*diff1_2+0.3*diff2_3);
+        scores.push(Math.random()+level3_score-0.3*diff1_3+0.3*diff2_3);
+        return scores.indexOf(Math.min(...scores))+1;
+    }
 }
 
+
+/**
+ * Function to collect genre data of UserData element
+ * @returns {GenreData} The sum of the two numbers
+ */
 function extractRelevantGenreData(data: UserData, genre: Genre){
-    const overallScore = data.progress.overall_score;
-    const overallQuestions = data.progress.overall_questions;
-    const overallGenreScore = data.progress.genre_scores[genre];
-    const overallGenreQuestions = data.progress.genres[genre].overall_questions;
-
-
-
+    // const overallScore = data.progress.overall_score;
+    // const overallQuestions = data.progress.overall_questions;
+    // const overallGenreScore = data.progress.genre_scores[genre];
+    // const overallGenreQuestions = data.progress.genres[genre].overall_questions;
+    // const historyGenreScore = data.progress.genres[genre].history_score;
+    // const currentQuestionsInHistory = data.progress.shortterm_genre_history.list_of_genre.filter((g)=>g===genre).length;
+    return{
+        overallScore : data.progress.overall_score,
+        overallQuestions : data.progress.overall_questions,
+        overallGenreScore :  data.progress.genre_scores[genre],
+        overallGenreQuestions : data.progress.genres[genre].overall_questions,
+        historyGenreScore : data.progress.genres[genre].history_score,
+        currentQuestionsInHistory : data.progress.shortterm_genre_history.list_of_genre.filter((g)=>g===genre).length
+    }
 }
 
