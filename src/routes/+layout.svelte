87459bd7e6dae7cf4aa23f72f7bdbd5853e53440
Yousri Cherif <>
2023-07-03 17:45:46 +0200

    Navbar updated

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,14 +5,14 @@
 	import '../app.postcss';
 
 
-	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+	import { AppBar, AppShell, ListBox, ListBoxItem} from '@skeletonlabs/skeleton';
 	import { onMount } from 'svelte';
 	import { auth, db } from '$lib/firebase/firebase';
 	import { doc, getDoc, setDoc, type DocumentData } from 'firebase/firestore';
 	import { authHandler, authStore } from '../store/store';
 	import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
-storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+	storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 
 	onMount(() => {
 		const unsubscribe = auth.onAuthStateChanged(async (user) => {
@@ -62,23 +62,70 @@ storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 		authHandler.logout();
 	};
 
+
+	import { popup } from '@skeletonlabs/skeleton';
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
+	let comboboxValue: string;
+
+	const popupCombobox: PopupSettings = {
+		event: 'focus-click',
+		target: 'popupCombobox',
+		placement: 'bottom',
+		closeQuery: '.listbox-item'
+	};
+
 </script>
+
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<AppBar>
+		<style>
+			.navbarr {
+				background-color: #ffffff; 
+			}
+		</style>
+		<AppBar class="navbarr px-8 shadow-sm">
 			<svelte:fragment slot="lead">
-				<img class="h-6 w-6 mr-4" src="/images/Facebook_logo_(square).png" alt="Logo"/>
-				<a class="text-xl font-bold" href="/">Know Your Music</a>
+				<!--<img class="h-6 w-6 mr-4" src="/images/Facebook_logo_(square).png" alt="Logo"/> -->
+				<a class="text-[#775AFF] text-2xl font-bold" href="/">Know Your Music</a>
 			</svelte:fragment>			
-			<nav>
-				<a class="btn btn-sm variant-soft-surface" href="/quiz/popularity">Popularity Quiz</a>
-				<a class="btn btn-sm variant-soft-surface" href="/quiz/biography">Bio Quiz</a>
-				<a class="btn btn-sm variant-soft-surface" href="/quiz/discography">Discography Quiz</a>
-				<a class="btn btn-sm variant-soft-surface" href="/stats">Stats</a>
-			</nav>
 			<svelte:fragment slot="trail">
-				<button on:click={logout} class="btn btn-sm variant-ringed-surface"> Logout </button>
+				<nav>
+					<button class="rounded-xl h-8 px-4 justify-between bg-[#ffffff] hover:bg-[#E9E9E9] duration-300" use:popup={popupCombobox}>
+						<span class="capitalize">{comboboxValue ?? 'Quiz'}</span>
+						<span>↓</span>
+					</button>
+					<div class="card shadow-xl variant-soft-surface" data-popup="popupCombobox">
+						<ListBox rounded="rounded-none">
+							<a href="/quiz/popularity">
+								<ListBoxItem bind:group={comboboxValue} name="medium" value="Popularity">
+									Popularity
+								</ListBoxItem>
+							</a>
+							<a href="/quiz/biography">
+								<ListBoxItem bind:group={comboboxValue} name="medium" value="Bio">
+									Bio
+								</ListBoxItem>
+							</a>
+							<a href="/quiz/discography">
+								<ListBoxItem bind:group={comboboxValue} name="medium" value="Discography">
+									Discography
+								</ListBoxItem>
+							</a>							
+						</ListBox>
+						<div class="arrow bg-surface-100-800-token" />
+					</div>
+					<button class="rounded-xl h-8 px-4 bg-[#ffffff] hover:bg-[#E9E9E9] duration-300">
+						<a href="/stats">Stats</a>
+					</button>
+					<button class="rounded-xl h-8 px-4 bg-[#ffffff] hover:bg-[#E9E9E9] duration-300">
+						<a href="">Team</a>
+					</button>
+					<button class="rounded-xl h-8 px-4 bg-[#ffffff] hover:bg-[#E9E9E9] duration-300">
+						<a href="">About</a>
+					</button>
+				</nav>
+				<button on:click={logout} class="text-white rounded-xl h-8 px-4 bg-[#775AFF] hover:bg-[#6A4AFF] duration-300"> Logout </button>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
@@ -87,3 +134,5 @@ storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
 		<slot />
 	</main>
 </AppShell>
+
+
