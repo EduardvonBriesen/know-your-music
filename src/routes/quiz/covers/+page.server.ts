cd10b2030618aff4f2b4987b5a94332df9278492
Louisa Ullmann <louisaullmann@web.de>
2023-07-10 18:29:54 +0200

    Antwort wird jetzt richtig bewertet.

--- src/routes/quiz/covers/+page.server.ts
+++ src/routes/quiz/covers/+page.server.ts
@@ -9,134 +9,141 @@ import { doc, getDoc, setDoc, type DocumentData } from 'firebase/firestore';
 const numberOfAlbums = 3;
 
 export const load = async ({ cookies }) => {
-	const current_quiz = JSON.parse(cookies.get('covers') || '{}');
+  const current_quiz = JSON.parse(cookies.get('covers') || '{}');
 
-	let artistName = current_quiz?.artist?.name;
-	let artistId = current_quiz?.artist?.id;
-	const albumIds = current_quiz?.albums;
+  let artistName = current_quiz?.artist?.name;
+  let artistId = current_quiz?.artist?.id;
+  const albumIds = current_quiz?.albums;
+  let currentAlbumName = current_quiz?.currentAlbumName;
 
-	if (!current_quiz.artist) {
-		// get random artist
-		const artist = await getRandomArtist();
+  if (!current_quiz.artist) {
+    // get random artist
+    const artist = await getRandomArtist();
 
-		// assert that artist is valid
-		if ('error' in artist)
-			return {
-				error: "Couldn't get artist"
-			};
+    // assert that artist is valid
+    if ('error' in artist)
+      return {
+        error: "Couldn't get artist"
+      };
 
-		artistName = artist.name;
-		artistId = artist.mbid;
-	}
-	// get spotify token
-	const spotifyToken = await getToken(cookies);
-	let spotifyId;
-	try {
-		spotifyId = await mbidToSpotifyId(artistId);
-	} catch (e) {
-		console.log(e);
-		return { error: "Couldn't get Spotify ID" };
-	}
+    artistName = artist.name;
+    artistId = artist.mbid;
+  }
 
-	if (!spotifyId) return { error: "Couldn't get Spotify ID" };
+  // get spotify token
+  const spotifyToken = await getToken(cookies);
+  let spotifyId;
+  try {
+    spotifyId = await mbidToSpotifyId(artistId);
+  } catch (e) {
+    console.log(e);
+    return { error: "Couldn't get Spotify ID" };
+  }
 
-	//spotify get Artist
-	const spotifyArtist = await getArtist(spotifyToken, spotifyId);
-	if ('error' in spotifyArtist) return { error: "Couldn't get artist" };
-	const artistImage = spotifyArtist.images[0].url;
+  if (!spotifyId) return { error: "Couldn't get Spotify ID" };
 
-	let albums: string[] | Album[] = [];
-	if (!albumIds) {
-		const artistAlbums = await getArtistAlbums(spotifyToken, spotifyArtist.id);
-		if ('error' in artistAlbums) return { error: "Couldn't get artist albums" };
+  // spotify get Artist
+  const spotifyArtist = await getArtist(spotifyToken, spotifyId);
+  if ('error' in spotifyArtist) return { error: "Couldn't get artist" };
+  const artistImage = spotifyArtist.images[0].url;
 
-		// FIlter out any names that contain the name of another album (gets rid of deluxe editions, etc.)
-		artistAlbums.items = artistAlbums.items.filter((album) => {
-			const albumName = album.name.toLowerCase();
-			return !artistAlbums.items.some((otherAlbum) => {
-				const otherAlbumName = otherAlbum.name.toLowerCase();
-				return albumName.includes(otherAlbumName) && albumName !== otherAlbumName;
-			});
-		});
+  let albums = [];
+  if (!albumIds) {
+    const artistAlbums = await getArtistAlbums(spotifyToken, spotifyArtist.id);
+    if ('error' in artistAlbums) return { error: "Couldn't get artist albums" };
 
-		// get random albums
-		artistAlbums.items.sort(() => Math.random() - 0.5);
+    // Filter out any names that contain the name of another album (gets rid of deluxe editions, etc.)
+    artistAlbums.items = artistAlbums.items.filter((album) => {
+      const albumName = album.name.toLowerCase();
+      return !artistAlbums.items.some((otherAlbum) => {
+        const otherAlbumName = otherAlbum.name.toLowerCase();
+        return albumName.includes(otherAlbumName) && albumName !== otherAlbumName;
+      });
+    });
 
-		albums = artistAlbums.items.slice(0, numberOfAlbums);
-	} else {
-		// get albums from cookie
-		const artistAlbums = await getSeveralAlbums(spotifyToken, albumIds);
-		if ('error' in artistAlbums) return { error: "Couldn't get artist albums" };
+    // get random albums
+    artistAlbums.items.sort(() => Math.random() - 0.5);
 
-		albums = artistAlbums.albums;
-	}
+    albums = artistAlbums.items.slice(0, numberOfAlbums);
 
-	// save quiz to cookie
-	cookies.set(
-		'covers',
-		JSON.stringify({
-			artist: {
-				name: artistName,
-				id: artistId
-			},
-			albums: albums.map((album) => album.id)
-		}),
-		{
-			path: '/'
-		}
-	);
+    // Setze den Namen des aktuellen Albums
+    currentAlbumName = albums[0].name;
+  } else {
+    // get albums from cookie
+    const artistAlbums = await getSeveralAlbums(spotifyToken, albumIds);
+    if ('error' in artistAlbums) return { error: "Couldn't get artist albums" };
 
-	return {
-		artist: {
-			name: artistName,
-			image: artistImage
-		},
-		albums: albums.map((album) => ({
-			id: album.id,
-			name: album.name,
-			image: album.images[0].url
-		}))
-	};
+    albums = artistAlbums.albums;
+
+    // Setze den Namen des aktuellen Albums, falls er nicht bereits gespeichert ist
+    if (!currentAlbumName) {
+      const currentAlbum = albums.find((album) => album.id === albumIds[0]);
+      currentAlbumName = currentAlbum?.name;
+    }
+  }
+
+  // save quiz to cookie
+  cookies.set(
+    'covers',
+    JSON.stringify({
+      artist: {
+        name: artistName,
+        id: artistId
+      },
+      albums: albums.map((album) => album.id),
+      currentAlbumName // Speichere den Namen des aktuellen Albums im Cookie
+    }),
+    {
+      path: '/'
+    }
+  );
+
+  return {
+    albums: albums.map((album) => ({
+      id: album.id,
+      name: album.name,
+      image: album.images[0].url
+    })),
+    currentAlbumName // Gebe den Namen des aktuellen Albums zurück
+  };
 };
 
 export const actions = {
 	default: async ({ request, cookies }) => {
-		const current_quiz = JSON.parse(cookies.get('covers') || '{}');
-
-		// evaluate answer
-		const answer = await request.formData();
-		const guess = answer.get('answer') as string;
-		console.log(guess);
-
-		const _albums = current_quiz.albums as string[];
-		console.log(_albums);
-
-		const spotifyToken = await getToken(cookies);
-		const albums = await getSeveralAlbums(spotifyToken, _albums);
-		console.log(albums);
-
-		// find current album
-		const currentAlbumName = albums.albums.find((album) => album.name === guess)?.name;
-		const correctAlbum = albums.albums.find((album) => album.name === currentAlbumName);
-		const isCorrect = correctAlbum?.name === guess;
-
-		console.log(currentAlbumName, correctAlbum, isCorrect);
-
-		// Benutzer aktualisieren
-		await updateUser(isCorrect, answer.get('user_id') as string);
-
-		// Quiz zurücksetzen
-		cookies.set('covers', '', {
-			path: '/'
-		});
-
-		if (!isCorrect) {
-			return fail(400, { false: guess, correct: correctAlbum?.name });
-		} else {
-			return fail(200, { false: null, correct: correctAlbum?.name });
-		}
+	  const current_quiz = JSON.parse(cookies.get('covers') || '{}');
+  
+	  // evaluate answer
+	  const answer = await request.formData();
+	  const guess = answer.get('answer') as string;
+  
+	  const spotifyToken = await getToken(cookies);
+	  const albums = await getSeveralAlbums(spotifyToken, current_quiz.albums);
+  
+	  // find current album
+	  const currentAlbumName = current_quiz.currentAlbumName; // Zugriff auf den zwischengespeicherten Namen des aktuellen Albums
+	  let correctAlbum;
+	  if ('albums' in albums) {
+		correctAlbum = albums.albums.find((album) => album.name === currentAlbumName);
+	  }
+  
+	  const isCorrect = correctAlbum?.name === guess;
+  
+	  // Benutzer aktualisieren
+	  await updateUser(isCorrect, answer.get('user_id') as string);
+  
+	  // Quiz zurücksetzen
+	  cookies.set('covers', '', {
+		path: '/'
+	  });
+  
+	  if (!isCorrect) {
+		return fail(400, { false: guess, correct: correctAlbum?.name });
+	  } else {
+		return fail(200, { false: null, correct: correctAlbum?.name });
+	  }
 	}
-};
+  };
+  
 const updateUser = async (correct: boolean, user_id: string) => {
 	const userRef = doc(db, 'users', user_id);
 	const userDoc = await getDoc(userRef);
