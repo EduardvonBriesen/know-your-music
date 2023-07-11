d9c09a73ffc510e93d954f490d392253beed5050
Eduard von Briesen <eduard.von-briesen@adesso.de>
2023-07-11 12:29:51 +0200

    feat: ⚡️ caching spotify api

--- src/lib/server/spotify.ts
+++ src/lib/server/spotify.ts
@@ -1,6 +1,7 @@
 import { VITE_SPOTIFY_CLIENT_ID, VITE_SPOTIFY_CLIENT_SECRET } from '$env/static/private';
 import type { Cookies } from '@sveltejs/kit';
 import type { Album, Artist, SpotifyError, Token, Track } from './spotify.types';
+import { redis } from './redis';
 
 const baseUrl = 'https://api.spotify.com/v1/';
 
@@ -36,47 +37,84 @@ export const getArtist = async (
 	token: string,
 	artist_id: string
 ): Promise<Artist | SpotifyError> => {
+	const query = 'spotify/artist/' + artist_id;
+	const cached = await redis.get(query);
+
+	if (cached) return cached;
+
 	const res = await fetch(`${baseUrl}artists/${artist_id}`, {
 		headers: {
 			Authorization: 'Bearer  ' + token
 		}
 	});
-	return await res.json();
+
+	const artist = await res.json();
+	redis.set(query, artist, 'EX', 60 * 60 * 24);
+
+	return artist;
 };
 
 export const getArtistTopTracks = async (
 	token: string,
 	artist_id: string
 ): Promise<{ tracks: Track[] } | SpotifyError> => {
+	const query = 'spotify/artist/top-tracks' + artist_id;
+	const cached = await redis.get(query);
+
+	if (cached) return cached;
+
 	const res = await fetch(`${baseUrl}artists/${artist_id}/top-tracks?market=DE`, {
 		headers: {
 			Authorization: 'Bearer  ' + token
 		}
 	});
-	return await res.json();
+
+	const topTracks = await res.json();
+	redis.set(query, topTracks, 'EX', 60 * 60 * 24);
+
+	return topTracks;
 };
 
 export const getRelatedArtists = async (
 	token: string,
 	artist_id: string
 ): Promise<{ artists: Artist[] } | SpotifyError> => {
+	const query = 'spotify/artist/related-artists' + artist_id;
+	const cached = await redis.get(query);
+
+	if (cached) return cached;
+
 	const res = await fetch(`${baseUrl}artists/${artist_id}/related-artists`, {
 		headers: {
 			Authorization: 'Bearer  ' + token
 		}
 	});
-	return await res.json();
+
+	const relatedArtists = await res.json();
+	redis.set(query, relatedArtists, 'EX', 60 * 60 * 24);
+
+	return relatedArtists;
 };
 
 export const getTracks = async (
 	token: string,
 	track_ids: string[]
 ): Promise<{ tracks: Track[] } | SpotifyError> => {
+	// TODO: cache individual tracks
+	const query = 'spotify/tracks/' + JSON.stringify(track_ids);
+	const cached = await redis.get(query);
+
+	if (cached) return cached;
+
 	const res = await fetch(`${baseUrl}tracks?ids=${track_ids.join(',')}`, {
 		headers: {
 			Authorization: 'Bearer  ' + token
 		}
 	});
+
+	const tracks = await res.json();
+	redis.set(query, tracks, 'EX', 60 * 60 * 24);
+
 	return await res.json();
 };
 
@@ -84,6 +122,11 @@ export const getArtistAlbums = async (
 	token: string,
 	artist_id: string
 ): Promise<{ items: Album[] } | SpotifyError> => {
+	const query = 'spotify/artist/albums' + artist_id;
+	const cached = await redis.get(query);
+
+	if (cached) return cached;
+
 	const url = new URL(`${baseUrl}artists/${artist_id}/albums`);
 	url.searchParams.append('include_groups', 'album');
 	const res = await fetch(url, {
@@ -91,17 +134,31 @@ export const getArtistAlbums = async (
 			Authorization: 'Bearer  ' + token
 		}
 	});
-	return await res.json();
+
+	const albums = await res.json();
+	redis.set(query, albums, 'EX', 60 * 60 * 24);
+
+	return albums;
 };
 
 export const getSeveralAlbums = async (
 	token: string,
 	album_ids: string[]
 ): Promise<{ albums: Album[] } | SpotifyError> => {
+	// TODO: cache individual albums
+	const query = 'spotify/albums/' + JSON.stringify(album_ids);
+	const cached = await redis.get(query);
+
+	if (cached) return cached;
+
 	const res = await fetch(`${baseUrl}albums?ids=${album_ids.join(',')}`, {
 		headers: {
 			Authorization: 'Bearer  ' + token
 		}
 	});
-	return await res.json();
+
+	const albums = await res.json();
+	redis.set(query, albums, 'EX', 60 * 60 * 24);
+
+	return albums;
 };
