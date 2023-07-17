854f0fc46173d55f8e31fb10680dd3394a78acd5
Yousri Cherif <>
2023-07-18 00:18:59 +0200

    Added "Song" quiz to onboarding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,23 +16,11 @@
 />
 
 <div class="flex justify-center items-center">
-	<div class="m-6 my-24 w-2/3 max-w-7xl">
-		<h2 class="text-3xl font-bold">Our quizzes</h2>
+	<div class="m-6 my-24 w-2/3 xl:w-1/2 max-w-7xl">
+		<h2 class="text-4xl font-bold flex justify-center mb-12">Our quizzes</h2>
 		<grid
-			class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6 mt-6"
+			class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 gap-6 mt-6"
 		>
-			<div class="flex flex-col">
-				<img
-					class="h-auto max-w-full object-cover rounded-md aspect-square shadow-lg"
-					src="/images/Popularity_quiz.png"
-					alt="Popularity"
-				/>
-				<p class="font-semibold mt-4">
-					Popularity. <span class="text-surface-400"
-						>How well do you know the most popular songs of your favorite artists?
-					</span>
-				</p>
-			</div>
 			<div class="flex flex-col">
 				<img
 					class="h-auto max-w-full object-cover rounded-md aspect-square shadow-lg"
@@ -40,21 +28,8 @@
 					alt="Biography"
 				/>
 				<p class="font-semibold mt-4">
-					Biography. <span class="text-surface-400">
+					Biography. <span class="text-surface-500">
 						How well do you know the life of your favorite artists? Let's find out and learn more
-					</span>
-				</p>
-			</div>
-			<div class="flex flex-col">
-				<img
-					class="h-auto max-w-full object-cover rounded-md aspect-square shadow-lg"
-					src="/images/Discography_quiz.png"
-					alt="Discography"
-				/>
-				<p class="font-semibold mt-4">
-					Discography. <span class="text-surface-400">
-						When were these albums released? Test your knowledge of your favorite artists'
-						discography
 					</span>
 				</p>
 			</div>
@@ -65,8 +40,21 @@
 					alt="Cover"
 				/>
 				<p class="font-semibold mt-4">
-					Cover. <span class="text-surface-400">
+					Cover. <span class="text-surface-500">
 						More of a visual learner? Test your knowledge of your favorite artists' album covers
+					</span>
+				</p>
+			</div>
+			<div class="flex flex-col">
+				<img
+					class="h-auto max-w-full object-cover rounded-md aspect-square shadow-lg"
+					src="/images/Discography_quiz.png"
+					alt="Discography"
+				/>
+				<p class="font-semibold mt-4">
+					Discography. <span class="text-surface-500">
+						When were these albums released? Test your knowledge of your favorite artists'
+						discography
 					</span>
 				</p>
 			</div>
@@ -77,8 +65,32 @@
 					alt="Lyrics"
 				/>
 				<p class="font-semibold mt-4">
-					Lyrics. <span class="text-surface-400">
+					Lyrics. <span class="text-surface-500">
 						How well do you know the lyrics of your favorite songs? Let's find out and sing along
+					</span>
+				</p>
+			</div>
+			<div class="flex flex-col">
+				<img
+					class="h-auto max-w-full object-cover rounded-md aspect-square shadow-lg"
+					src="/images/Popularity_quiz.png"
+					alt="Popularity"
+				/>
+				<p class="font-semibold mt-4">
+					Popularity. <span class="text-surface-500"
+						>How well do you know the most popular songs of your favorite artists?
+					</span>
+				</p>
+			</div>
+			<div class="flex flex-col">
+				<img
+					class="h-auto max-w-full object-cover rounded-md aspect-square shadow-lg"
+					src="/images/Song_quiz.png"
+					alt="Song"
+				/>
+				<p class="font-semibold mt-4">
+					Song. <span class="text-surface-500"
+						>Listen and guess! Identify tunes from top artists. Enjoy the musical challenge!
 					</span>
 				</p>
 			</div>
