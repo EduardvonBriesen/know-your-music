79a582060895fda428b31747b88ae5ea2ab5e936
Eduard von Briesen <eduard.von-briesen@adesso.de>
2023-07-18 05:59:14 +0200

    feat: 🚧 added remaining charts

--- src/lib/firebase/dashboardLoading.ts
+++ src/lib/firebase/dashboardLoading.ts
@@ -1,9 +1,25 @@
-import { doc, getDoc } from "firebase/firestore";
-import type { UserData, Genre, ScoreType, Levels, ScoreHistoryType, ItemTypes, ItemGenreType } from "./dataBase.types";
-import type {DonutChartOptions,ChartTabularData, RadarChartOptions, GaugeChartOptions, LineChartOptions, BarChartOptions } from '@carbon/charts-svelte';
+import { doc, getDoc } from 'firebase/firestore';
+import type {
+	UserData,
+	Genre,
+	ScoreType,
+	Levels,
+	ScoreHistoryType,
+	ItemTypes,
+	ItemGenreType
+} from './dataBase.types';
+import {
+	type DonutChartOptions,
+	type ChartTabularData,
+	type RadarChartOptions,
+	type GaugeChartOptions,
+	type LineChartOptions,
+	type BarChartOptions,
+	Alignments
+} from '@carbon/charts-svelte';
 import { db } from './firebase';
 
-export async function getGenreOrItemtypeQuestionsDonutChart(docName: string, type: ItemGenreType){
+export async function getGenreOrItemtypeQuestionsDonutChart(docName: string, type: ItemGenreType) {
 	const collectionsName = 'users';
 	const docRef = doc(db, collectionsName, docName);
 	const docSnap = await getDoc(docRef);
@@ -11,275 +27,277 @@ export async function getGenreOrItemtypeQuestionsDonutChart(docName: string, typ
 		return;
 	}
 	const userData: UserData = docSnap.data() as UserData;
-    const genres: Genre[] = ["rock", "classic", "folk_music", "jazz", "rap", "pop"];
-    const items: ItemTypes[] = ["Biography", "Coverguess", "Discography", "Lyrics", "Popularity"];
+	const genres: Genre[] = ['rock', 'classic', 'folk_music', 'jazz', 'rap', 'pop'];
+	const items: ItemTypes[] = ['Biography', 'Coverguess', 'Discography', 'Lyrics', 'Popularity'];
 
-    const data: ChartTabularData = [];
-    
-    if (type==="Genre"){
-        for(const genre of genres){
-            const dataElement = {
-                group: genre,
-                value: userData.progress.genres[genre].overall_questions
-            };
-            data.push(dataElement);
-        }
-    }else{ //type===Itemtype
-        for(const item of items){
-            const dataElement = {
-                group: item,
-                value: userData.progress.itemtypes[item].overallQuestions
-            };
-            data.push(dataElement);
-        }
-    }
+	const data: ChartTabularData = [];
 
-    const options: DonutChartOptions = {
-        "title": "Blub",
-        "resizable": true,
-        "donut": {
-            "center": {
-                "label": "Overall Questions"
-            }
-        },
-        "height": "400px"
-    };
+	if (type === 'Genre') {
+		for (const genre of genres) {
+			const dataElement = {
+				group: genre,
+				value: userData.progress.genres[genre].overall_questions
+			};
+			data.push(dataElement);
+		}
+	} else {
+		//type===Itemtype
+		for (const item of items) {
+			const dataElement = {
+				group: item,
+				value: userData.progress.itemtypes[item].overallQuestions
+			};
+			data.push(dataElement);
+		}
+	}
 
-    return {data, options};
+	const options: DonutChartOptions = {
+		title: 'Blub',
+		resizable: true,
+		donut: {
+			alignment: Alignments.CENTER,
+			center: {
+				label: 'Overall Questions'
+			}
+		},
+		height: '100%'
+	};
+
+	return { data, options };
 }
 
-export async function getGenreOrItemtypeScoresOverallAndHistoryRadarChart(docName: string, type: ItemGenreType){
-    const collectionsName = 'users';
+export async function getGenreOrItemtypeScoresOverallAndHistoryRadarChart(
+	docName: string,
+	type: ItemGenreType
+) {
+	const collectionsName = 'users';
 	const docRef = doc(db, collectionsName, docName);
 	const docSnap = await getDoc(docRef);
 	if (!docSnap.exists()) {
 		return;
 	}
 	const userData: UserData = docSnap.data() as UserData;
-    const genres: Genre[] = ["rock", "classic", "folk_music", "jazz", "rap", "pop"];
-    const items: ItemTypes[] = ["Biography", "Coverguess", "Discography", "Lyrics", "Popularity"];
+	const genres: Genre[] = ['rock', 'classic', 'folk_music', 'jazz', 'rap', 'pop'];
+	const items: ItemTypes[] = ['Biography', 'Coverguess', 'Discography', 'Lyrics', 'Popularity'];
 
-    const data: ChartTabularData = [];
-    if (type==="Genre"){
-        for(const genre of genres){
-            let dataElement = {
-                group: "Overall Score",
-                feature: genre,
-                value: userData.progress.genre_scores[genre]
-            };
-            data.push(dataElement);
-            dataElement = {
-                group: "History Score",
-                feature: genre,
-                value: userData.progress.genres[genre].history_score
-            };
-            data.push(dataElement);
-        }
-    }else{ //type===Itemtype
-        for(const item of items){
-            let dataElement = {
-                group: "Overall Score",
-                feature: item,
-                value: userData.progress.itemtypes[item].overallScore
-            };
-            data.push(dataElement);
-            dataElement = {
-                group: "History Score",
-                feature: item,
-                value: userData.progress.itemtypes[item].historyScore
-            };
-            data.push(dataElement);
-        }
-    }
+	const data: ChartTabularData = [];
+	if (type === 'Genre') {
+		for (const genre of genres) {
+			let dataElement = {
+				group: 'Overall Score',
+				feature: genre,
+				value: userData.progress.genre_scores[genre]
+			};
+			data.push(dataElement);
+			dataElement = {
+				group: 'History Score',
+				feature: genre,
+				value: userData.progress.genres[genre].history_score
+			};
+			data.push(dataElement);
+		}
+	} else {
+		//type===Itemtype
+		for (const item of items) {
+			let dataElement = {
+				group: 'Overall Score',
+				feature: item,
+				value: userData.progress.itemtypes[item].overallScore
+			};
+			data.push(dataElement);
+			dataElement = {
+				group: 'History Score',
+				feature: item,
+				value: userData.progress.itemtypes[item].historyScore
+			};
+			data.push(dataElement);
+		}
+	}
 
-    const options:RadarChartOptions = {
-        "title": "Genre Scores - Alltime vs. short-term History",
-        "radar": {
-            "axes": {
-                "angle": "feature", //dont change
-                "value": "value" // dont change
-            }
-        },
-        "height": "400px"
-    };
-    return {data, options}
+	const options: RadarChartOptions = {
+		title: 'Genre Scores - Alltime vs. short-term History',
+		radar: {
+			axes: {
+				angle: 'feature', //dont change
+				value: 'value' // dont change
+			}
+		}
+	};
+	return { data, options };
 }
 
-export async function getScoreGaugeChart(docName: string, scoreType:ScoreType ){
-    const collectionsName = 'users';
+export async function getScoreGaugeChart(docName: string, scoreType: ScoreType) {
+	const collectionsName = 'users';
 	const docRef = doc(db, collectionsName, docName);
 	const docSnap = await getDoc(docRef);
 	if (!docSnap.exists()) {
 		return;
 	}
 	const userData: UserData = docSnap.data() as UserData;
-    let score = 0;
-    let change = +5;
-    if(scoreType==='Overall'){
-        score = userData.progress.overall_score;
-        change = +5; // one moth back
-    }else{ // scoreType === 'History'
-        score = userData.progress.shortterm_overall_history.score;
-        change = +1; // one week back
-    }
+	let score = 0;
+	let change = +5;
+	if (scoreType === 'Overall') {
+		score = userData.progress.overall_score;
+		change = +5; // one moth back
+	} else {
+		// scoreType === 'History'
+		score = userData.progress.shortterm_overall_history.score;
+		change = +1; // one week back
+	}
 
-    const data: ChartTabularData = [
-        {
-            "group": "value",
-            "value": score*100
-        },
-        {
-            "group": "delta",
-            "value": change // tbd
-        }
-    ];
+	const data: ChartTabularData = [
+		{
+			group: 'value',
+			value: score * 100
+		},
+		{
+			group: 'delta',
+			value: change // tbd
+		}
+	];
 
-    const options: GaugeChartOptions = {
-        "title": "Gauge semicircular -- danger status",
-        "resizable": true,
-        "height": "250px",
-        "width": "100%",
-        "gauge": {
-          "type": "semi",//GaugeTypes.SEMI,
-          "status": 'danger'  // Statuses.DANGER
-        },
-        //"theme": "g10"
-    };
+	const options: GaugeChartOptions = {
+		title: 'Gauge semicircular -- danger status',
+		resizable: true,
+		height: '250px',
+		width: '100%',
+		gauge: {
+			type: 'semi', //GaugeTypes.SEMI,
+			status: 'danger' // Statuses.DANGER
+		}
+		//"theme": "g10"
+	};
 
-    return {data, options};
-
+	return { data, options };
 }
 
-export async function getLevelGenreVerticalGroupedBarChart(docName: string){
-    const collectionsName = 'users';
-    const docRef = doc(db, collectionsName, docName);
+export async function getLevelGenreVerticalGroupedBarChart(docName: string) {
+	const collectionsName = 'users';
+	const docRef = doc(db, collectionsName, docName);
 	const docSnap = await getDoc(docRef);
 	if (!docSnap.exists()) {
 		return;
 	}
 	const userData: UserData = docSnap.data() as UserData;
-    const genres: Genre[] = ["rock", "classic", "folk_music", "jazz", "rap", "pop"];
-    const levels: Levels[] = ["level1", "level2", "level3"]
+	const genres: Genre[] = ['rock', 'classic', 'folk_music', 'jazz', 'rap', 'pop'];
+	const levels: Levels[] = ['level1', 'level2', 'level3'];
 
-    const data: ChartTabularData = [];
+	const data: ChartTabularData = [];
 
-    for (const level of levels){
-        for (const genre of genres){
-            const dataElement = {
-                "group": level,
-                "key": genre,
-                "value": userData.progress.genres[genre][level].correct
-            }
-            data.push(dataElement);
-        }
-    }
+	for (const level of levels) {
+		for (const genre of genres) {
+			const dataElement = {
+				group: level,
+				key: genre,
+				value: userData.progress.genres[genre][level].correct
+			};
+			data.push(dataElement);
+		}
+	}
 
-    const options:BarChartOptions  = {
-        "title": "Vertical grouped bar (discrete)",
-        "axes": {
-            "left": {
-                "mapsTo": "value"
-            },
-            "bottom": {
-                "scaleType": "labels",
-                "mapsTo": "key"
-            }
-        },
-        "height": "400px",
-        //"theme": "g100"
-      };
+	const options: BarChartOptions = {
+		title: 'Vertical grouped bar (discrete)',
+		axes: {
+			left: {
+				mapsTo: 'value'
+			},
+			bottom: {
+				scaleType: 'labels',
+				mapsTo: 'key'
+			}
+		}
+		//"theme": "g100"
+	};
 
-      return {
-        data: data, 
-        options: options
-    };
-
+	return {
+		data: data,
+		options: options
+	};
 }
 
-export async function getScoresHistoryLineChart(docName: string, type: ScoreHistoryType){
-    const collectionsName = 'users';
-    const docRef = doc(db, collectionsName, docName);
+export async function getScoresHistoryLineChart(docName: string, type: ScoreHistoryType) {
+	const collectionsName = 'users';
+	const docRef = doc(db, collectionsName, docName);
 	const docSnap = await getDoc(docRef);
 	if (!docSnap.exists()) {
 		return;
 	}
 	const userData: UserData = docSnap.data() as UserData;
 
-    const data: ChartTabularData = [];
-    let scoresArray: number[] = [];
-    let datasetName= "";
-    let itr = 1;
-    const genres: Genre[] = ["rock", "classic", "folk_music", "jazz", "rap", "pop"];
-    const items: ItemTypes[] = ["Biography", "Coverguess", "Discography", "Lyrics", "Popularity"]
+	const data: ChartTabularData = [];
+	let scoresArray: number[] = [];
+	let datasetName = '';
+	let itr = 1;
+	const genres: Genre[] = ['rock', 'classic', 'folk_music', 'jazz', 'rap', 'pop'];
+	const items: ItemTypes[] = ['Biography', 'Coverguess', 'Discography', 'Lyrics', 'Popularity'];
 
-    if (type==="Genre"){
-        for(const genre of genres){
-            scoresArray= userData.progress.genres[genre].history.scores;
-            datasetName= `Genre Score - ${genre}`;
-            itr = 1;
-            for (const score  of scoresArray ){
-                const newElement = {
-                    "group": datasetName,
-                    "key": itr.toString(),
-                    "value": score,
-                };
-                data.push(newElement);
-                itr = itr+1;
-            }
-            itr = 1;
-        }
-    } else if (type==="Itemtype"){
-        for(const item of items){
-            scoresArray= userData.progress.itemtypes[item].historyScores
-            datasetName= `Itemtype Score - ${item}`;
-            itr = 1;
-            for (const score  of scoresArray ){
-                const newElement = {
-                    "group": datasetName,
-                    "key": itr.toString(),
-                    "value": score,
-                };
-                data.push(newElement);
-                itr = itr+1;
-            }
-            itr = 1;
-        }
-    } else {
-        scoresArray = userData.progress.shortterm_overall_history.list_of_scores;
-        datasetName= "Overall Score";
-        let itr = 1;
-        for (const score  of scoresArray ){
-            const newElement = {
-                "group": datasetName,
-                "key": itr.toString(),
-                "value": score,
-            };
-            data.push(newElement);
-            itr = itr+1;
-        }
-    }
+	if (type === 'Genre') {
+		for (const genre of genres) {
+			scoresArray = userData.progress.genres[genre].history.scores;
+			datasetName = `Genre Score - ${genre}`;
+			itr = 1;
+			for (const score of scoresArray) {
+				const newElement = {
+					group: datasetName,
+					key: itr.toString(),
+					value: score
+				};
+				data.push(newElement);
+				itr = itr + 1;
+			}
+			itr = 1;
+		}
+	} else if (type === 'Itemtype') {
+		for (const item of items) {
+			scoresArray = userData.progress.itemtypes[item].historyScores;
+			datasetName = `Itemtype Score - ${item}`;
+			itr = 1;
+			for (const score of scoresArray) {
+				const newElement = {
+					group: datasetName,
+					key: itr.toString(),
+					value: score
+				};
+				data.push(newElement);
+				itr = itr + 1;
+			}
+			itr = 1;
+		}
+	} else {
+		scoresArray = userData.progress.shortterm_overall_history.list_of_scores;
+		datasetName = 'Overall Score';
+		let itr = 1;
+		for (const score of scoresArray) {
+			const newElement = {
+				group: datasetName,
+				key: itr.toString(),
+				value: score
+			};
+			data.push(newElement);
+			itr = itr + 1;
+		}
+	}
 
-    const options:LineChartOptions = {
-        "title": "Line (discrete)",
-        "axes": {
-            "bottom": {
-                "title": "2019 Annual Sales Figures",
-                "mapsTo": "key",
-                "scaleType": "labels"
-            },
-            "left": {
-                "mapsTo": "value",
-                "title": "Conversion rate",
-                "scaleType": "linear"
-            }
-        },
-        "height": "400px",
-        //"theme": "g100"    
-    };
+	const options: LineChartOptions = {
+		title: 'Line (discrete)',
+		axes: {
+			bottom: {
+				title: '2019 Annual Sales Figures',
+				mapsTo: 'key',
+				scaleType: 'labels'
+			},
+			left: {
+				mapsTo: 'value',
+				title: 'Conversion rate',
+				scaleType: 'linear'
+			}
+		}
+		// "height": "400px",
+		//"theme": "g100"
+	};
 
-    return {
-        data: data, 
-        options:options
-    };
-
+	return {
+		data: data,
+		options: options
+	};
 }
--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -1,10 +1,15 @@
 <script async script lang="ts">
-	import { LineChart } from '@carbon/charts-svelte';
+	import { BarChartGrouped, DonutChart, GaugeChart, LineChart, RadarChart } from '@carbon/charts-svelte';
 	import '@carbon/styles/css/styles.css'; // may affect global styles
 	import '@carbon/charts-svelte/styles.css';
 	import type { UserData, Genre } from '../../lib/firebase/dataBase.types';
 	import { initDataStructure } from '../../lib/firebase/initDemoUser1';
-	import { getScoresHistoryLineChart } from '../../lib/firebase/dashboardLoading';
+	import {
+		getGenreOrItemtypeQuestionsDonutChart,
+		getLevelGenreVerticalGroupedBarChart,
+		getScoreGaugeChart,
+		getScoresHistoryLineChart
+	} from '../../lib/firebase/dashboardLoading';
 	import { authStore } from '../../store/store';
 
 	let user_id: string = '';
@@ -13,33 +18,47 @@
 		user_id = store.user.uid;
 	});
 
-	const dataPromise = getScoresHistoryLineChart(user_id, 'Genre');
+	const donutDataGenre = getGenreOrItemtypeQuestionsDonutChart(user_id, 'Genre');
+	const radarDataGenre = getGenreOrItemtypeQuestionsDonutChart(user_id, 'Genre');
+	const gaugeDataGenre = getScoreGaugeChart(user_id, 'History');
+	const barDataGenre = getLevelGenreVerticalGroupedBarChart(user_id);
+	const lineDataGenre = getScoresHistoryLineChart(user_id, 'Genre');
 </script>
 
 <div class="flex justify-center items-center">
 	<div class="w-3/4 md:w-4/5 xl:w-2/3 m-12 h-4/5">
 		<div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-			<div
-				class="card variant-soft-surface p-4 text-token space-4 flex place-content-center aspect-square"
-			>
-				<p>Chart 1</p>
+			<div class="card variant-soft-surface p-4">
+				{#await donutDataGenre}
+					<p>Loading...</p>
+				{:then data}
+					<DonutChart data={data.data} options={data.options} />
+				{/await}
 			</div>
 			<div
 				class="card variant-soft-surface p-4 text-token space-4 flex place-content-center aspect-square"
 			>
-				<p>Chart 2</p>
+				{#await radarDataGenre}
+					<p>Loading...</p>
+				{:then data}
+					<RadarChart data={data.data} options={data.options} />
+				{/await}
 			</div>
 			<div
 				class="card variant-soft-surface p-4 text-token space-4 flex place-content-center aspect-square"
 			>
-				<p>Chart 3</p>
+				{#await gaugeDataGenre}
+					<p>Loading...</p>
+				{:then data}
+					<GaugeChart data={data.data} options={data.options} />
+				{/await}
 			</div>
 		</div>
 		<div class="mt-4 grid grid-cols-1 lg:grid-cols-2 gap-4 h-1/2">
 			<div
 				class="card variant-soft-surface p-4 text-token space-4 flex place-content-center aspect-video"
 			>
-				{#await dataPromise}
+				{#await lineDataGenre}
 					<p>Loading...</p>
 				{:then data}
 					<LineChart data={data.data} options={data.options} />
@@ -48,7 +67,11 @@
 			<div
 				class="card variant-soft-surface p-4 text-token space-4 flex place-content-center aspect-video"
 			>
-				<p>Chart 5</p>
+				{#await barDataGenre}
+					<p>Loading...</p>
+				{:then data}
+					<BarChartGrouped data={data.data} options={data.options} />
+				{/await}
 			</div>
 		</div>
 	</div>
