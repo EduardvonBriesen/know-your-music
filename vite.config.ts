1002337cc163cfe7face518a0de8d0facf7dd30c
Eduard von Briesen <eduard.von-briesen@adesso.de>
2023-07-18 05:18:16 +0200

    chore: ➕ added carbon charts dependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: process.env.NODE_ENV === 'production' ? ['@carbon/charts', 'carbon-components'] : []
+	}
 });
